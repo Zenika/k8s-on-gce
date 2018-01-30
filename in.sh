9e8d3baa5317c905246504829b3de1c99176baa3
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-01-30 09:36:15 +0100

    🔧 Remove unnecessary volume

--- in.sh
+++ in.sh
@@ -6,7 +6,6 @@ if [[ $? -eq 0 ]]; then
     docker rm -f terra 
     
     docker run -it \
-        -v $PWD/../tools/gcloud:/root/.config/gcloud \
         -v $PWD/app:/root/app \
         --name terra terra
 fi
