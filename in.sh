f4e75405743764a2d9ba6e57b86f2f999b45ad43
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-02-16 23:57:47 +0100

    🔨 Renaming tool image & container

--- in.sh
+++ in.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-docker build . -t tools
+docker build . -t k8s-on-gce/tools
 
 if [[ $? -eq 0 ]]; then
-    docker rm -f tools 
+    docker rm -f k8s-on-gce-tools 
     
     docker run -it \
         -v $PWD/app:/root/app \
         -p 8001:8001 \
-        --name tools tools
+        --name k8s-on-gce-tools k8s-on-gce/tools
 fi
