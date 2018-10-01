85b861d818b27f9e22d4250c532875a8d8d17bdc
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-10-01 15:02:39 +0200

    Upgrade to k8s 1.12 & containerd 1.12-rc0

--- app/create.sh
+++ app/create.sh
@@ -3,7 +3,7 @@
 export ETCD_VERSION=v3.3.5
 export CNI_VERSION=0.3.1
 export CNI_PLUGINS_VERSION=v0.6.0
-export CONTAINERD_VERSION=1.1.0
+export CONTAINERD_VERSION=1.2.0-rc.0
 
 rm -f /root/.ssh/google_compute_engine*
 # ⚠️ Here we create a key with no passphrase
